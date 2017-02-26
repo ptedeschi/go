51e78fa96e0d8f721f4c828ac168a13f01d7c4cd
Patrick Tedeschi <patrick@tedeschi.com.br>
2017-02-26 08:50:45 -0300

    Initialize database insertion Add logging to debug Htttp traffic

--- source/android/DiapersGo/app/src/main/java/br/com/tedeschi/diapersgo/activity/AddDealActivity.java
+++ source/android/DiapersGo/app/src/main/java/br/com/tedeschi/diapersgo/activity/AddDealActivity.java
@@ -1,24 +1,36 @@
 package br.com.tedeschi.diapersgo.activity;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 
 import br.com.tedeschi.diapersgo.R;
+import br.com.tedeschi.diapersgo.model.Deals;
+import br.com.tedeschi.diapersgo.rest.ApiClient;
+import br.com.tedeschi.diapersgo.rest.ApiInterface;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AddDealActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    EditText venueName;
     Spinner spinner1;
     Spinner spinner2;
+    private String TAG = "AddDealActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_deal);
 
+        venueName = (EditText) findViewById(R.id.editTextVenue);
         spinner1 = (Spinner) findViewById(R.id.spinner_diaper_brand);
          spinner2 = (Spinner) findViewById(R.id.spinner_diaper_model);
 
@@ -46,5 +58,41 @@ public class AddDealActivity extends AppCompatActivity implements AdapterView.On
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+
+    public void selfDestruct(View view) {
+
+        ApiInterface apiService =
+                ApiClient.getClient().create(ApiInterface.class);
+
+        Call<Deals> call = apiService.insert("VenueFoursquareId", venueName.getText().toString(), "VenueAddress", "VenueCity", "SP", "VenueCountry", "-22.852876134654547", "-47.02767489301763", "CustomerFirstName", "CustomerLastName", "CustomerEmail", "CustomerCountry", "ProductName", "ProductBrand", "ProductModel", "G", "64", "55.55", "DealType", "DealComment");
+        call.enqueue(new Callback<Deals>() {
+
+            /**
+             * Invoked for a received HTTP response.
+             * <p>
+             * Note: An HTTP response may still indicate an application-level failure such as a 404 or 500.
+             * Call {@link Response#isSuccessful()} to determine if the response indicates success.
+             *
+             * @param call
+             * @param response
+             */
+            @Override
+            public void onResponse(Call<Deals> call, Response<Deals> response) {
+                Log.d("TAG", "onResponse");
+            }
+
+            /**
+             * Invoked when a network exception occurred talking to the server or when an unexpected
+             * exception occurred creating the request or processing the response.
+             *
+             * @param call
+             * @param t
+             */
+            @Override
+            public void onFailure(Call<Deals> call, Throwable t) {
+                Log.e("TAG", "onFailure", t);
+            }
+        });
     }
 }
--- source/android/DiapersGo/app/src/main/java/br/com/tedeschi/diapersgo/rest/ApiInterface.java
+++ source/android/DiapersGo/app/src/main/java/br/com/tedeschi/diapersgo/rest/ApiInterface.java
@@ -10,4 +10,7 @@ import retrofit2.http.Query;
 public interface ApiInterface {
     @GET("venues/byCoordinate")
     Call<Deals> getTopRatedMovies(@Query("lat") double lat, @Query("lng") double lng);
+
+    @GET("venues/insert")
+    Call<Deals> insert(@Query("VenueFoursquareId") String VenueFoursquareId, @Query("VenueName") String VenueName, @Query("VenueAddress") String VenueAddress, @Query("VenueCity") String VenueCity, @Query("VenueState") String VenueState, @Query("VenueCountry") String VenueCountry, @Query("VenueLatitude") String VenueLatitude, @Query("VenueLongitude") String VenueLongitude, @Query("CustomerFirstName") String CustomerFirstName, @Query("CustomerLastName") String CustomerLastName, @Query("CustomerEmail") String CustomerEmail, @Query("CustomerCountry") String CustomerCountry, @Query("ProductName") String ProductName, @Query("ProductBrand") String ProductBrand, @Query("ProductModel") String ProductModel, @Query("ProductSize") String ProductSize, @Query("ProductQuantity") String ProductQuantity, @Query("DealPrice") String DealPrice, @Query("DealType") String DealType, @Query("DealComment") String DealComment);
 }
