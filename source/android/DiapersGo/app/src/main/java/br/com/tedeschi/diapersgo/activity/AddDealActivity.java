e04e78165afff44016e1b32b6a4fa9241a670df2
Patrick Tedeschi <patrick@tedeschi.com.br>
2017-02-26 21:32:30 -0300

    Adding search venues functionality

--- source/android/DiapersGo/app/src/main/java/br/com/tedeschi/diapersgo/activity/AddDealActivity.java
+++ source/android/DiapersGo/app/src/main/java/br/com/tedeschi/diapersgo/activity/AddDealActivity.java
@@ -1,6 +1,5 @@
 package br.com.tedeschi.diapersgo.activity;
 
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,7 +8,12 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.AutocompleteFilter;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 
 import br.com.tedeschi.diapersgo.R;
 import br.com.tedeschi.diapersgo.model.Deals;
@@ -20,38 +24,71 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class AddDealActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-    EditText venueName;
-    Spinner spinner1;
-    Spinner spinner2;
+    PlaceAutocompleteFragment venueName;
+    Spinner spinner_diaper_brand;
+    Spinner spinner_diaper_model;
+    Spinner spinner_diaper_size;
     private String TAG = "AddDealActivity";
+    private Place mPlace;
+    private EditText mNumberOfDiapers;
+    private EditText mDiaperPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_deal);
 
-        venueName = (EditText) findViewById(R.id.editTextVenue);
-        spinner1 = (Spinner) findViewById(R.id.spinner_diaper_brand);
-         spinner2 = (Spinner) findViewById(R.id.spinner_diaper_model);
+        venueName = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+        spinner_diaper_brand = (Spinner) findViewById(R.id.spinner_diaper_brand);
+         spinner_diaper_model = (Spinner) findViewById(R.id.spinner_diaper_model);
+        spinner_diaper_size = (Spinner) findViewById(R.id.spinner_diaper_size);
 
-        spinner1.setOnItemSelectedListener(this);
+        mNumberOfDiapers = (EditText) findViewById(R.id.editText2);
+        mDiaperPrice = (EditText) findViewById(R.id.editText3);
+
+        venueName.setHint("Estabelecimento");
+
+        AutocompleteFilter typeFilter = new AutocompleteFilter.Builder()
+                .setCountry("BR").setTypeFilter(AutocompleteFilter.TYPE_FILTER_ESTABLISHMENT)
+                .build();
+
+        venueName.setFilter(typeFilter);
+
+        venueName.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.getName());
+
+                mPlace = place;
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+
+        spinner_diaper_brand.setOnItemSelectedListener(this);
     }
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        if (spinner1.getSelectedItem().equals("Pampers")) {
+        if (spinner_diaper_brand.getSelectedItem().equals("Pampers")) {
 
             ArrayAdapter adapter2 = ArrayAdapter.createFromResource(this,
                     R.array.diaper_brand_pampers_array, android.R.layout.simple_list_item_1);
-            spinner2.setAdapter(adapter2);
-        } else if (spinner1.getSelectedItem().equals("Huggies")) {
+            spinner_diaper_model.setAdapter(adapter2);
+        } else if (spinner_diaper_brand.getSelectedItem().equals("Huggies")) {
             ArrayAdapter adapter2 = ArrayAdapter.createFromResource(this,
                     R.array.diaper_brand_huggies_array, android.R.layout.simple_list_item_1);
-            spinner2.setAdapter(adapter2);
-        } else if (spinner1.getSelectedItem().equals("Mamypoko")) {
+            spinner_diaper_model.setAdapter(adapter2);
+        } else if (spinner_diaper_brand.getSelectedItem().equals("Mamypoko")) {
             ArrayAdapter adapter2 = ArrayAdapter.createFromResource(this,
                     R.array.diaper_brand_mamypoko_array, android.R.layout.simple_list_item_1);
-            spinner2.setAdapter(adapter2);
+            spinner_diaper_model.setAdapter(adapter2);
         }
     }
 
@@ -65,7 +102,7 @@ public class AddDealActivity extends AppCompatActivity implements AdapterView.On
         ApiInterface apiService =
                 ApiClient.getClient().create(ApiInterface.class);
 
-        Call<Deals> call = apiService.insert("VenueFoursquareId", venueName.getText().toString(), "VenueAddress", "VenueCity", "SP", "VenueCountry", "-22.852876134654547", "-47.02767489301763", "CustomerFirstName", "CustomerLastName", "CustomerEmail", "CustomerCountry", "ProductName", "ProductBrand", "ProductModel", "G", "64", "55.55", "DealType", "DealComment");
+        Call<Deals> call = apiService.insert(mPlace.getId(), mPlace.getName().toString(), mPlace.getAddress().toString(), "", "", "", Double.toString(mPlace.getLatLng().latitude), Double.toString(mPlace.getLatLng().longitude), "CustomerFirstName", "CustomerLastName", "CustomerEmail", "CustomerCountry", spinner_diaper_brand.getSelectedItem().toString(), spinner_diaper_model.getSelectedItem().toString(), "ProductModel", spinner_diaper_size.getSelectedItem().toString(), mNumberOfDiapers.getText().toString(), mDiaperPrice.getText().toString(), "DealType", "DealComment");
         call.enqueue(new Callback<Deals>() {
 
             /**
